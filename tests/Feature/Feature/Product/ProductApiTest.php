7ce5d47cf0dd8dbdeb72ac05aa48d78b9122191e
myathtut-zafir <aa.htut@gmail.com>
2025-06-10 12:00:00 +0700

    feat: product create test case

--- tests/Feature/Feature/Product/ProductApiTest.php
+++ tests/Feature/Feature/Product/ProductApiTest.php
@@ -26,11 +26,11 @@ class ProductApiTest extends TestCase
 
     #[Test] public function an_admin_can_create_a_product()
     {
-        $admin =User::factory()->admin()->create();
+        $admin = User::factory()->admin()->create();
         $productData = Product::factory()->make()->toArray();
 
         $response = $this->actingAs($admin, 'sanctum')
-        ->postJson('/api/products', $productData);
+            ->postJson('/api/products', $productData);
 
         $response->assertStatus(201)
             ->assertJsonFragment([
@@ -42,5 +42,18 @@ class ProductApiTest extends TestCase
             'name' => $productData['name'],
             'price' => $productData['price'],
         ]);
+    }
+
+    #[Test] public function a_regular_user_can_not_create_a_product()
+    {
+        $user = User::factory()->create();
+        $productData = Product::factory()->make()->toArray();
+
+        $response = $this->actingAs($user, 'sanctum')
+            ->postJson('/api/products', $productData);
+
+        $response->assertStatus(403);
+        $this->assertDatabaseMissing('products', ['name' => $productData['name']]);
+    
     }
 }
